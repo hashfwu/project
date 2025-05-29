b3bcfd250621ca5758a194c96417af231e03beb4
hashfwu <134555886+hashfwu@users.noreply.github.com>
2025-05-28 21:18:31 -0400

    First commit

---
+++ App SQL Server/config.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    'driver': '{ODBC Driver 18 for SQL Server}',
+    'server': 'localhost',
+    'database': 'Bomberos',
+    'trusted_connection': 'yes',
+    'trust_server_certificate': 'yes',
+    # PARA LA AUTENTICACIÓN DE WINDOWS
+    'username': 'sa',
+    'password': '123456SQL.',
+}
