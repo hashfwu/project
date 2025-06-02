28466d602b56073e281eadccd9c6f904fb061927
hashfwu <134555886+hashfwu@users.noreply.github.com>
2025-06-01 22:23:48 -0400

    Creando nueva ruta

---
+++ app/database/connection.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pyodbc
+from app.config import DB_CONFIG
+
+app = Flask(__name__)
+
+
+def get_connection():
+    # Windows Autentication
+    # conn = pyodbc.connect(
+    #     f"DRIVER={DB_CONFIG['driver']};"
+    #     f"SERVER={DB_CONFIG['server']};"
+    #     f"DATABASE={DB_CONFIG['database']};"
+    #     f"Trusted_Connection={DB_CONFIG['trusted_connection']};"
+    #     f"TrustServerCertificate={DB_CONFIG['trust_server_certificate']};"
+    # )
+    
+    # SQL Server Autentication
+    conn = pyodbc.connect(
+        f"DRIVER={DB_CONFIG['driver']};"
+        f"SERVER={DB_CONFIG['server']};"
+        f"DATABASE={DB_CONFIG['database']};"
+        f"UID={DB_CONFIG['username']};"
+        f"PWD={DB_CONFIG['password']};"
+        f"TrustServerCertificate={DB_CONFIG['trust_server_certificate']};"
+    )
+    return conn
